36732e44d3ab4d95ddae8a0278db6376c39c8692
Beau Hankins <beau.j.hankins@gmail.com>
2015-05-07 06:48:10 +1000

    infosite hero & nav sections

--- src/templates/infosite/javascripts/main.js
+++ src/templates/infosite/javascripts/main.js
@@ -1,3 +1,18 @@
 $(function() {
+  $(window).scroll(function (event) {
+    var scroll        = $(window).scrollTop();
+    var heroHeight    = $(".hero").height();
+    var nav           = $(".nav");
 
+    if (scroll >= heroHeight) {
+      nav.css({
+        "position":"fixed",
+        "top":"0"
+      });
+    } else {
+      nav.css({
+        "position":"initial"
+      });
+    }
+  });
 });
