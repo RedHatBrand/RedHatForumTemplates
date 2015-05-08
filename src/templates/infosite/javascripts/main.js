ac60bf1d5d03f46e2bd9f14c698984512a361150
Beau Hankins <beau.j.hankins@gmail.com>
2015-05-08 11:44:32 +1000

    infosite progress: hero + nav

--- src/templates/infosite/javascripts/main.js
+++ src/templates/infosite/javascripts/main.js
@@ -1,18 +1,48 @@
 $(function() {
+  var body           = $("body");
+
+  var BREAKPOINTS = [200,400,600,800];
+
   $(window).scroll(function (event) {
     var scroll        = $(window).scrollTop();
-    var heroHeight    = $(".hero").height();
-    var nav           = $(".nav");
 
-    if (scroll >= heroHeight) {
-      nav.css({
-        "position":"fixed",
-        "top":"0"
-      });
-    } else {
-      nav.css({
-        "position":"initial"
-      });
+    updateNavPosition();
+    updateNavActiveItem();
+
+    function updateNavPosition() {
+      var heroHeight    = $(".hero").height();
+      var nav           = $(".nav");
+
+      if (scroll >= heroHeight) {
+        nav.addClass("sticky-nav");
+        body.css({ "margin-top": nav.height() });
+      } else {
+        nav.removeClass("sticky-nav");
+        body.css({ "margin": 0 });
+      }
     }
+
+    function updateNavActiveItem() {
+
+      if (scroll < BREAKPOINTS[0]) {
+        $("ul.nav-list li").removeClass("active");
+        return;
+      }
+
+      for (var i = BREAKPOINTS.length - 1; i >= 0; i--) {
+        var y = BREAKPOINTS[i];
+
+        if (scroll >= y) {
+          $("ul.nav-list li").removeClass("active");
+          $("ul.nav-list li:nth-child(" + i + ")").addClass("active");
+          return;
+        }
+      };
+    }
+  });
+
+  $("ul.nav-list li a").click(function() {
+    i = $("ul.nav-list li a").index(this);
+    body.animate({ scrollTop: BREAKPOINTS[i + 1] }, 500);
   });
 });
