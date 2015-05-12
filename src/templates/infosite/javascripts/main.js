46c9127de43dd3a7e933e3c83b93c7984917a0a8
Beau Hankins <beau.j.hankins@gmail.com>
2015-05-12 17:14:15 +1000

    Agenda

--- src/templates/infosite/javascripts/main.js
+++ src/templates/infosite/javascripts/main.js
@@ -1,19 +1,30 @@
 $(function() {
   var body            = $("body");
-  var heroHeight      = $(".hero").height();
+  var nav             = $(".nav");
+  var hero            = $(".hero");
 
-  var BREAKPOINTS = [0, 0, 100, 200];
+  var BREAKPOINTS;
+
+  updateBreakpoints = function() { 
+    var heroHeight             = hero.height();
+    var agendaTop              = sectionTop(".section-agenda");
+
+    BREAKPOINTS = [heroHeight, heroHeight, agendaTop];
+  }
+
+  updateBreakpoints();
+
+  $(window).bind('resize', updateBreakpoints);
 
   $(window).scroll(function (e) {
-    var scroll        = $(window).scrollTop();
+    var scroll = $(window).scrollTop();
 
     updateNavPosition();
     updateNavActiveItem();
 
     function updateNavPosition() {
-      var nav           = $(".nav");
 
-      if (scroll >= heroHeight) {
+      if (scroll >= hero.height()) {
         nav.addClass("sticky-nav");
         body.css({ "margin-top": nav.height() });
       } else {
@@ -30,20 +41,45 @@ $(function() {
       }
 
       for (var i = BREAKPOINTS.length - 1; i >= 0; i--) {
-        var y = heroHeight + BREAKPOINTS[i];
+        var y = BREAKPOINTS[i];
 
         if (scroll >= y) {
           $("ul.nav-list li").removeClass("active");
           $("ul.nav-list li:nth-child(" + i + ")").addClass("active");
+
+          if (i == 1) { $(".agenda-session").addClass("animated fadeInUp"); }
+
           return;
         }
       };
     }
   });
 
-  $("ul.nav-list li a").click(function() {
+  $("ul.nav-list li a").click(function(e) {
+    e.preventDefault();
+
     i = $("ul.nav-list li a").index(this);
-    var y = heroHeight + BREAKPOINTS[i + 1]
+    var y = BREAKPOINTS[i + 1]
     body.animate({ scrollTop: y }, 1000);
+  });
+
+  function sectionTop(section) {
+    var trueTop         = $(section).position().top;
+
+    return trueTop - nav.height();
+  }
+
+  if ($("[name='active-day']").length > 0) {
+    (function selectFirstDay() {
+
+      $(".agenda-container-session").css({"display":"none"});
+      $(".agenda-container-session#agenda-session-"+$("[name='active-day']").first().attr('id')).css({"display":"block"});
+      return $("[name='active-day']").first().attr('checked', 'checked');
+    })();
+  }
+
+  $(".agenda-input-day[type='radio']").click(function() {
+    $(".agenda-container-session").css({"display":"none"});
+    $(".agenda-container-session#agenda-session-"+$(this).attr('id')).css({"display":"block"});
   });
 });
